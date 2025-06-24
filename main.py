208e66ec392551f9111dcb5489f93971fdc0f08b
PrxBxny <konotop.ilya1304@gmail.com>
2025-06-24 06:24:58 +0300

    final

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
 import os
 
 def split_of_type(folder:list):
-    text = ['txt', 'doc', 'docx', 'rtf', 'odt']
-    pictures = ['jpg', 'png', 'gif', 'tiff']
-    audio = ['wav', 'mp3', 'aif', 'aiff', 'flac', 'mid']
-    spreadsheets = ['xls', 'xlsx', 'ods', 'dbf']
-    programming = ['py', 'cpp', 'js', 'java', 'c', 'cs', 'go']
-
-    text_paths = []
-    pictures_paths = []
-    audio_paths = []
-    spreadsheets_paths = []
-    programming_paths = []
+    extensions = {'text': ['txt', 'doc', 'docx', 'rtf', 'odt'],
+                  'pictures': ['jpg', 'png', 'gif', 'tiff'],
+                  'audio': ['wav', 'mp3', 'aif', 'aiff', 'flac', 'mid'],
+                  'spreadsheets': ['xls', 'xlsx', 'ods', 'dbf', 'csv'],
+                  'programming': ['py', 'cpp', 'c', 'cs', 'js', 'java', 'go']
+    }
+    files = {'text_files': [], 'pictures_files': [], 'audio_files': [],
+             'spreadsheets_files': [], 'programming_files': []
+    }
 
     for file in folder:
-        print(file)
-        if file.split('.')[-1] in text:
-            text_paths.append(file)
-        elif file.split('.')[-1] in pictures:
-            pictures_paths.append(file)
-        elif file.split('.')[-1] in audio:
-            audio_paths.append(file)
-        elif file.split('.')[-1] in spreadsheets:
-            spreadsheets_paths.append(file)
-        elif file.split('.')[-1] in programming:
-            programming_paths.append(file)
+        if file.split('.')[-1] in extensions['text']:
+            files['text_files'].append(file)
+        elif file.split('.')[-1] in extensions['pictures']:
+            files['pictures_files'].append(file)
+        elif file.split('.')[-1] in extensions['audio']:
+            files['audio_files'].append(file)
+        elif file.split('.')[-1] in extensions['spreadsheets']:
+            files['spreadsheets_files'].append(file)
+        elif file.split('.')[-1] in extensions['programming']:
+            files['programming_files'].append(file)
+        else: return None
 
-    return[text_paths, pictures_paths, audio_paths, spreadsheets_paths, programming_paths]
+    return files
 
 
 def sort(path:str):
     if os.path.isdir(path):
         files = split_of_type(os.listdir(path))
-        for i in range(len(files)):
-            print(files[i])
+        if files:
+            for keys in files:
+                if files[keys]:
+                    os.mkdir(f'{path}/{keys}')
+                    for file in files.get(keys):
+                        os.replace(f'{path}/{file}', f'{path}/{keys}/{file}')
+            print('Done!')
+        else:
+            print('Apparently there is nothing to sort')
     else:
-        print('файлов не обнаружено')
+        print('ERROR. sorter need a dir path')
+
+def main():
+    path = input('Input path to your folder. I will sorted it: ')
+    sort(path)
+
+while True:
+    main()
